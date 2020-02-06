7271aedd4d45b897e82409f57481c9fef66ea1d3
Berniece Buttermilk <bbutterm@at-q6.21-school.ru>
2020-02-06 21:34:41 +0300

    dasd

--- mybot.py
+++ mybot.py
@@ -8,16 +8,22 @@ bot = telebot.TeleBot('995380002:AAH-LvUX2Cm9aiUGXAKcF777NyAYSUpFMKA')
 apihelper.proxy = {'https':'https://167.86.80.2:3128'}
 conn = sql.connect('db.db',check_same_thread=False)
 cursor = conn.cursor()
-conn.commit()
 cursor.execute("select id from users")
 ids = cursor.fetchall()
-print(ids)
+conn.commit()
+buff = []
+for i in ids:
+    buff.append(i[0])
+print (buff)
 @bot.message_handler(content_types=['text'])
 def start(message):
-    if message.from_user.id not in ids:
+    if (message.from_user.id) not in buff:
         bot.send_message(message.from_user.id, "Whats your name?");
         bot.register_next_step_handler(message, register);
-    #else
+    else:
+        cursor.execute("select name from users where id = message.from_user.id")
+        username = cursor.fetchall()
+        bot.send_message(message.from_user.id,{}, "Hello", username);
 def register(message):
     userid = message.from_user.id;
     username = message.text;
