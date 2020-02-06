85a7b947d78a56383b53c027f61e5f0891947479
Berniece Buttermilk <bbutterm@at-q6.21-school.ru>
2020-02-06 22:16:18 +0300

    asdas

--- mybot.py
+++ mybot.py
@@ -5,7 +5,7 @@ from telebot import apihelper
 import sqlite3 as sql
 from telebot import types
 bot = telebot.TeleBot('995380002:AAH-LvUX2Cm9aiUGXAKcF777NyAYSUpFMKA')
-apihelper.proxy = {'https':'https://167.86.80.2:3128'}
+apihelper.proxy = {'https':'https://64.227.2.136:8080'}
 conn = sql.connect('db.db',check_same_thread=False)
 cursor = conn.cursor()
 cursor.execute("select id from users")
@@ -15,15 +15,20 @@ buff = []
 for i in ids:
     buff.append(i[0])
 print (buff)
+cursor.execute("select * from users");
+base = cursor.fetchall();
+print(base)
 @bot.message_handler(content_types=['text'])
 def start(message):
     if (message.from_user.id) not in buff:
         bot.send_message(message.from_user.id, "Whats your name?");
         bot.register_next_step_handler(message, register);
     else:
-        cursor.execute("select name from users where id = message.from_user.id")
+        iden = int(message.from_user.id)
+        cursor.execute("select name from users where id = iden")
         username = cursor.fetchall()
-        bot.send_message(message.from_user.id,{}, "Hello", username);
+        print (username)
+        bot.send_message(message.from_user.id,f'Hello {username}');
 def register(message):
     userid = message.from_user.id;
     username = message.text;
